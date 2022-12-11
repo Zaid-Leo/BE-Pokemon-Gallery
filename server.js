12f7097147cbac74adce9e0ca2053220eff5e171
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-11 18:10:18 +0100

    server done

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const server = express();
 const port = 9999;
 
 server.use(express.json());
+
+server.use("/pokemons", pokemonRouter);
+
+server.use((req, res) => {
+    res.status(404).send("This site is not existant");
+});
+
+server.use((err, req, res, next) => {
+    console.log("An error occured", err);
+    res.status(500).send("The server has an error");
+});
+
+server.listen(port, () => {
+    console.log("Server is running on port: " + port);
+});
