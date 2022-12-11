78671f93a8859b75b40e86114530288bf0532ee7
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-11 21:54:30 +0100

    port running again

--- controller/pokemonController.js
+++ controller/pokemonController.js
@@ -3,8 +3,63 @@ import { JSONFile } from "lowdb/node";
 
 const db = new Low(new JSONFile("data/db.json"));
 
-export const getAllPokemons = async (req, res) => {
+export const getAllCards = async (req, res) => {
     await db.read();
 
     res.json(db.data.Cards);
+};
+export const getCard = async (req, res) => {
+    await db.read();
+    const value = db.data.Cards.find((a) => a.id === +req.params.id);
+
+    if (!value) {
+        res.status(404).send("Not found");
+        return;
+    }
+    res.json(value);
+};
+
+export const editCard = async (req, res) => {
+    await db.read();
+
+    const index = db.data.Cards.findIndex((a) => a.id === +req.params.id);
+
+    if (index < 0) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    db.data.Cards[index] = { ...db.data.Cards[index], ...req.body };
+
+    await db.write();
+
+    res.send(`${req.params.id} updated`);
+};
+
+export const deleteCard = async (req, res) => {
+    await db.read();
+    const index = db.data.Cards.findIndex((a) => a.id === +req.params.id);
+
+    if (index < 0) {
+        res.status(404).send("Not found");
+        return;
+    }
+
+    db.data.Cards.splice(index, 1);
+
+    db.write();
+
+    res.status(202).send(`${req.params.id} deleted`);
+};
+
+export const saveCard = async (req, res) => {
+    await db.read();
+
+    const nextId = Math.max(...db.data.Cards.map((a) => a.id)) + 1;
+
+    db.data.Cards.push({ id: nextId, ...req.body });
+
+    db.write();
+
+    res.send(`${nextId}`);
 };
--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -3,5 +3,10 @@ import * as controller from "../controller/pokemonController.js";
 
 const router = express.Router();
 
-router.get("/", controller.getAllPokemons);
+router
+    .get("/", controller.getAllCards)
+    .get("/:id", controller.getCard)
+    .put("/:id", controller.editCard)
+    .delete("/:id", controller.deleteCard)
+    .post("/", controller.saveCard);
 export default router;
